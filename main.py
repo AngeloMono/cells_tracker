5d1fecff2753ed425aa0f5fc6e58ffc04570b45a
AngeloMono <angelo.mono1996@gmail.com>
2021-01-06 17:49:18 +0100

    Implement Sort tracker and add debug functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
-from numpy import random
+from numpy import random, empty, array
 
 from models.experimental import attempt_load
 from utils.datasets import LoadImages, LoadWebcam
@@ -11,83 +11,124 @@ from utils.general import check_img_size, non_max_suppression, scale_coords
 from utils.plots import plot_one_box
 from utils.torch_utils import time_synchronized
 
+from sort import Sort
+
 # Source path video or 0 to camera
-source = r"C:\Users\angel\Desktop\Video Dimauro\Video cellula sola\Trai-test video\sola (3).mp4"
+source = r"C:\Users\angel\Desktop\Video Dimauro\Video cellule multiple in movimento\Train-test video\multi (2).mp4"
+
+debug = True
+wait_key = 0 if debug else 1
+proportion_vid = 0.25 if debug else 0.5
 
 # Yolov5 variables
-weights = r"C:\Users\angel\Desktop\Video Dimauro\Dataset 1\Results\train yolov5s ep700 imgsize416 sd1\results\content\yolov5\runs\train\yolov5s_results\weights\best.pt"
-img_size = 416
-conf_thres = 0.25
-iou_thres = 0.45
+weights_yolo = r"C:\Users\angel\Desktop\Video Dimauro\Dataset 1\Results\train yolov5s ep700 imgsize416 sd1\results\content\yolov5\runs\train\yolov5s_results\weights\best.pt"
+img_size_yolo = 416
+conf_thres_yolo = 0.40
+iou_thres_yolo = 0.2
+
+# SORT variables
+max_age_sort = 20  # Maximum number of frames to keep alive a track without associated detections
+min_hits_sort = 5  # Minimum number of associated detections before track is initialised
+iou_thres_sort = 0.000002
+
+# Create instance of SORT
+mot_tracker = Sort(max_age=max_age_sort, min_hits=min_hits_sort, iou_threshold=iou_thres_sort)
+tracks = empty((0, 5))
 
 # Get source properties
 vidcap = cv2.VideoCapture(source)
 fps = vidcap.get(cv2.CAP_PROP_FPS)
 
-# Initalize
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # Torch device used
-half = device.type != 'cpu'  # Half precision  only supported on CUDA
+with torch.no_grad():
+    # Initalize
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # Torch device used
+    half = device.type != 'cpu'  # Half precision  only supported on CUDA
 
-# Load model
-model = attempt_load(weights, map_location=device)  # load FP32 model
-imgsz = check_img_size(img_size, s=model.stride.max())  # Check img_size
-if half:
-    model.half()  # To FP16 (only supported on CUDA)
+    # Load model
+    model = attempt_load(weights_yolo, map_location=device)  # load FP32 model
+    imgsz = check_img_size(img_size_yolo, s=model.stride.max())  # Check img_size
+    if half:
+        model.half()  # To FP16 (only supported on CUDA)
 
-# Extract names and assign a color RGB to each name
-names = model.module.names if hasattr(model, 'module') else model.names
-colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    # Extract names and assign a color RGB to each name
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
-# Load video
-if str(device) == '0':
-    cudnn.benchmark = True  # Set True to speed up constant image size inference
-    video = LoadWebcam(img_size=img_size)
-else:
-    video = LoadImages(source, img_size=img_size)
+    # Load video
+    if str(source) == '0':
+        cudnn.benchmark = True  # Set True to speed up constant image size inference
+        video = LoadWebcam(img_size=img_size_yolo)
+    else:
+        video = LoadImages(source, img_size=img_size_yolo)
 
-# Detect for each frame in video
-for path, img, im0s, vid_cap in video:
-    img = torch.from_numpy(img).to(device)
-    img = img.half() if half else img.float()  # uint8 to fp16/32
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    # Detect for each frame in video
+    for path, img, im0s, vid_cap in video:  # And print (tot_frame\current_frame) while call the __next__method
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
 
-    if img.ndimension() == 3:
-        img = img.unsqueeze(0)
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
 
-    # Inference
-    t1 = time_synchronized()
-    pred = model(img, augment=False)[0]
+        # Inference
+        t1 = time_synchronized()
+        detections = model(img, augment=False)[0]
 
-    # Apply NMS
-    pred = non_max_suppression(pred, conf_thres, iou_thres, classes=None, agnostic=False)
-    t2 = time_synchronized()
+        # Apply NMS
+        detections = non_max_suppression(detections, conf_thres_yolo, iou_thres_yolo, classes=None, agnostic=False)
+        detections = detections[0]
+        t2 = time_synchronized()
 
-    # Process detections predicted
-    for i, det in enumerate(pred):
-        if str(device) == '0':  # Batch_size >= 1
-            p, s, im0 = Path(path[i]), '%g: ' % i, im0s[i].copy()
-        else:
-            p, s, im0 = Path(path), '', im0s
-
+        # Processing detections predicted
+        p, s, im0 = Path(path), '', array(im0s, copy=True)
         s += '%gx%g ' % img.shape[2:]  # Print string
 
-        if len(det):
+        if len(detections):
             # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+            detections[:, :4] = scale_coords(img.shape[2:], detections[:, :4], im0.shape).round()
+
+            # SORT tracker application
+            np_det = detections.cpu().numpy()
+            tracks = mot_tracker.update(np_det[:, 0:-1])
 
             # Print results
-            for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # Detections for class
+            for c in detections[:, -1].unique():
+                n = (detections[:, -1] == c).sum()  # Detections for class
                 s += '%g %ss, ' % (n, names[int(c)])  # Add number and name of detection to string
 
-            # Write results on im0
-            for *xyxy, conf, cls in reversed(det):
-                label = '%s %.2f' % (names[int(cls)], conf)  # Create label for detection
-                plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3) # Add plot (xy,xy) to img
+        else:
+            tracks = mot_tracker.update(empty((0, 5)))
+
+        # Write tracks on im0
+        for x1, y1, x2, y2, id_track in reversed(tracks):
+            label = f'{names[0]} n. {int(id_track)}'
+            plot_one_box((x1, y1, x2, y2), im0, label=label, color=colors[0], line_thickness=3)
+
+        print(f'{s}Done. ({(t2 - t1):.3f}s) nt({len(tracks)})')
 
         # Stream results
-        cv2.imshow(str(p), im0)
-        if cv2.waitKey(1) & 0xFF == 27:  # Press esc to exit
-            raise StopIteration
+        if debug:
+            # Write yolo detections on im0_yolo
+            im0_yolo = array(im0s, copy=True)
+            im0_all_tracks = array(im0s, copy=True)
+            for *xyxy, conf, cls in reversed(detections):
+                label = '%s %.2f' % (names[int(cls)], conf)
+                plot_one_box(xyxy, im0_yolo, label=label, color=colors[int(cls)], line_thickness=3)
 
-        print('%sDone. (%.3fs)' % (s, t2 - t1))
+            # Write all tracks from SORT tracker on img0_all_tracks
+            for t in mot_tracker.trackers:
+                state = t.get_state()[0]
+                id_track = t.id
+                plot_one_box(state, im0_all_tracks, label=str(id_track), color=colors[int(cls)], line_thickness=3)
+
+            # Stream  debug results img
+            im0_yolo = cv2.resize(im0_yolo, (0, 0), fx=proportion_vid, fy=proportion_vid)
+            im0_all_tracks = cv2.resize(im0_all_tracks, (0, 0), fx=proportion_vid, fy=proportion_vid)
+            cv2.imshow('all tracks', im0_all_tracks)
+            cv2.imshow('yolo detection', im0_yolo)
+
+        # Stream out-put results img
+        im0 = cv2.resize(im0, (0, 0), fx=proportion_vid, fy=proportion_vid)
+        cv2.imshow(str(p), im0)
+        if cv2.waitKey(wait_key) & 0xFF == 27:  # Press esc to exit
+            raise StopIteration
